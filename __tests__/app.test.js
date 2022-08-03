e142bb44dc46c1cca9331de445ca83f83b6c9503
ccccc170 <ccccc170@protonmail.com>
2022-08-03 11:40:05 +0100

    this commit includes completed ticket 4 including error handling for 404: valid but non-existent id and 400: invalid id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -48,3 +48,49 @@ describe("GET /api/topics", () => {
     });
   });
 });
+
+describe("GET /api/articles/:article_id", () => {
+  describe("Successful usage", () => {
+    test("Status 200: should return an article object with 'author', 'title', 'article_id', 'body', 'topic', 'created_at' and 'votes' properties", () => {
+      const articleId = 1;
+      return request(app)
+        .get(`/api/articles/${articleId}`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(typeof body).toBe("object");
+          expect(body !== null).toBe(true);
+          expect(!Array.isArray(body)).toBe(true);
+          expect(Object.keys(body).length).toBe(7);
+          expect(body).toEqual(
+            expect.objectContaining({
+              article_id: articleId,
+              title: "Living in the shadow of a great man",
+              topic: "mitch",
+              author: "butter_bridge",
+              body: "I find this existence challenging",
+              created_at: expect.any(String),
+              votes: 100,
+            })
+          );
+        });
+    });
+  });
+  describe("errors", () => {
+    test("GET:404 sends an appropriate and error message when given a valid but non-existent id", () => {
+      return request(app)
+        .get("/api/articles/99999")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe("article not found!");
+        });
+    });
+    test("GET:400 sends an appropriate and error message when given an invalid id", () => {
+      return request(app)
+        .get("/api/articles/not-an-id")
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.msg).toBe("invalid request!");
+        });
+    });
+  });
+});
---
+++ models/articles.models.js
@@ -0,0 +1,14 @@
+const db = require("../db/connection");
+
+exports.selectArticleById = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    .then((response) => {
+      const { rows: row } = response;
+      if (row.length === 0) {
+        return Promise.reject({ status: 404, msg: "article not found!" });
+      }
+      const [article] = row;
+      return article;
+    });
+};
