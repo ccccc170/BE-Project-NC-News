628ba6955b63f12b5ecb523b02590276538ceb55
ccccc170 <ccccc170@protonmail.com>
2022-08-03 16:20:43 +0100

    this commit includes completed ticket 5 including error handling

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,8 +1,23 @@
-const { selectArticleById } = require("../models/articles.models");
+const {
+  selectArticleById,
+  updateArticleById,
+} = require("../models/articles.models");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   selectArticleById(article_id)
+    .then((article) => {
+      res.status(200).send(article);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.patchArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  const { inc_votes: editVotesBy } = req.body;
+  updateArticleById(article_id, editVotesBy)
     .then((article) => {
       res.status(200).send(article);
     })
