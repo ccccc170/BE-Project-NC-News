94830613cb6941b4130c0f5da6ef65f1e65f7b6f
ccccc170 <ccccc170@protonmail.com>
2022-08-13 13:53:29 +0100

    this commit is up to ticket 11

--- models/articles.models.js
+++ models/articles.models.js
@@ -3,7 +3,7 @@ const db = require("../db/connection");
 exports.selectArticles = () => {
   return db
     .query(
-      `SELECT articles.*, COUNT (comments.article_id) ::INT AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id;`
+      `SELECT articles.*, COUNT (comments.article_id) ::INT AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC;`
     )
     .then(({ rows: articles }) => {
       return articles;
