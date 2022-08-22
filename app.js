78227e3bc3f883ec97942cf09e7a39d136f96e36
ccccc170 <ccccc170@protonmail.com>
2022-08-22 09:55:49 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 const { getAllApi } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controllers");
